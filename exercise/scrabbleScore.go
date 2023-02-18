c2d922cbebcd8f0293d72576146fcdddb96b1033
armiinjp <arminjp2000@gmail.com>
2023-02-18 22:36:22 +0330

    Solving Exercise

--- exercise/scrabbleScore.go
+++ exercise/scrabbleScore.go
@@ -7,14 +7,14 @@ func Score(word string) int {
 	totalPoint := 0
 	word = strings.ToUpper(word)
 	points := map[string]int{
-							"A, E, I, O, U, L, N, R, S, T": 1,
-							"D, G": 2,
-							"B, C, M, P": 3,
-							"F, H, V, W, Y": 4,
-							"K": 5,
-							"J, X": 8,
-							"Q, Z": 10,
-						}
+			"A, E, I, O, U, L, N, R, S, T": 1,
+			"D, G": 2,
+			"B, C, M, P": 3,
+			"F, H, V, W, Y": 4,
+			"K": 5,
+			"J, X": 8,
+			"Q, Z": 10,
+			}
 	for _, char := range word{
 		for key, value := range points{
 			if strings.Contains(key, string(char)){
