6ce99f76c150e8b52cfbec43ae214b08d3a6b518
armiinjp <arminjp2000@gmail.com>
2023-02-24 21:31:46 +0330

    create company_agency.go app

--- Project/company_agency.go
+++ Project/company_agency.go
@@ -1,120 +1,79 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"log"
-	"strconv"
-	"bufio"
 	"os"
+	"strconv"
 	"strings"
 )
 
-type Agency struct{
-	Id	int
-	Name string
-	Address string
-	phoneNumber string
-	registeryDate string
+type Agency struct {
+	Id             int
+	Name           string
+	Address        string
+	phoneNumber    string
+	registeryDate  string
 	numberEmployee int
 }
 
 var regionAgencies map[string][]Agency
 
-func checkFlagRegionAndExist(inputRegion string) (string, error){
+func checkFlagRegionAndExist(inputRegion string) (string, error) {
 	fRegion, err := checkFlagRegion(inputRegion)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// check region is Exist:
-	if _, ok := regionAgencies[fRegion]; !ok{
+	if _, ok := regionAgencies[fRegion]; !ok {
 		return "", fmt.Errorf("region %s is not Exist!\n", fRegion)
 	}
 	return fRegion, nil
 }
-func checkFlagRegion(inputRegion string) (string, error){
+func checkFlagRegion(inputRegion string) (string, error) {
 	var flagRegion string
 	if inputRegion == "" {
 		fmt.Printf("You Don't enter region name, Please Enter Specific Region\nRegion Name: ")
 		fmt.Scanln(&flagRegion)
-		
-		if flagRegion == ""{
+
+		if flagRegion == "" {
 			return "", fmt.Errorf("region Name is Empty!\n")
 		}
-	} else {flagRegion = inputRegion}
+	} else {
+		flagRegion = inputRegion
+	}
 
 	return flagRegion, nil
 }
-func list(region string) ([]Agency){
+func list(region string) []Agency {
 	return regionAgencies[region]
 }
-func get(region string, id int) (Agency, error){
-	for _, v := range regionAgencies[region]{
-		if v.Id == id {return v, nil}
+func get(region string, id int) (Agency, error) {
+	for _, v := range regionAgencies[region] {
+		if v.Id == id {
+			return v, nil
+		}
 	}
-	return Agency{}, fmt.Errorf("Agency with Id: %d is not Found!", id)  //what is the better code ???
-}	
-func create(region string, agency Agency ) error{
+	return Agency{}, fmt.Errorf("Agency with Id: %d is not Found!", id) //what is the better code ???
+}
+func create(region string, agency Agency) error {
 	file, err := os.OpenFile("./data.txt", os.O_APPEND|os.O_WRONLY, 0600)
-    if err != nil {
-        return err
-    }
-    defer file.Close()
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
 	newLine := fmt.Sprintf("%s,%d,%s,%s,%s,%s,%d\n",
 		region, agency.Id, agency.Name, agency.Address, agency.phoneNumber, agency.registeryDate, agency.numberEmployee)
-    if _, err = file.WriteString(newLine); err != nil {
+	if _, err = file.WriteString(newLine); err != nil {
 		return err
 	}
-	
+
 	return nil
 }
-func edit(region string, id int, agency Agency) error{
-	file, err := os.OpenFile("data.txt",os.O_RDONLY, 0600)
-	if err != nil {
-		return err
-	}
-	fileScanner := bufio.NewScanner(file)
-
-	fileScanner.Split(bufio.ScanLines)
- 
-	var existId string
-	var tmpLine string
-	var editedFile []string
-
-	for fileScanner.Scan() {
-		tmpLine = fileScanner.Text() + "\n"
-		existId = strings.Split(tmpLine, ",")[1]
-		if existId == strconv.Itoa(id){
-			tmpLine = fmt.Sprintf("%s,%d,%s,%s,%s,%s,%d\n",
-			region, agency.Id, agency.Name, agency.Address, agency.phoneNumber, agency.registeryDate, agency.numberEmployee)			
-		}
-		editedFile = append(editedFile, tmpLine) 
-	}
-
-	file.Close()
-	os.Remove("data.txt")
-
-	file, err = os.OpenFile("./data.txt",os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		return err
-	}
-	for _, v := range editedFile{
-		if _, err = file.WriteString(v); err != nil {
-			return err
-		}
-	}
-	file.Close()
-	return nil
-}
-func status(region string) (int, int){
-	numberAllEmploye := 0
-	for _, v := range regionAgencies[region]{
-		numberAllEmploye += v.numberEmployee
-	}
-	return len(regionAgencies[region]), numberAllEmploye
-}
-func textFileToMap() error {
+func edit(region string, id int, agency Agency) error {
 	file, err := os.OpenFile("data.txt", os.O_RDONLY, 0600)
 	if err != nil {
 		return err
@@ -122,53 +81,106 @@ func textFileToMap() error {
 	fileScanner := bufio.NewScanner(file)
 
 	fileScanner.Split(bufio.ScanLines)
-	
-	var tmpSlice []string
-	var newListAgency []Agency
-	var tmpAgency Agency
-	
-	for fileScanner.Scan() {
-		tmpSlice = strings.Split(fileScanner.Text(), ",")
-		tmpAgency.Id, err = strconv.Atoi(tmpSlice[1]);if err != nil {return err}
-		tmpAgency.Name = tmpSlice[2]
-		tmpAgency.Address = tmpSlice[3]
-		tmpAgency.phoneNumber = tmpSlice[4]
-		tmpAgency.registeryDate = tmpSlice[5]
-		tmpAgency.numberEmployee, err = strconv.Atoi(tmpSlice[6]);if err != nil {return err}
 
-		if _, ok := regionAgencies[tmpSlice[0]]; ok{
-			regionAgencies[tmpSlice[0]] = append(regionAgencies[tmpSlice[0]], tmpAgency)
-		} else {
-			newListAgency = append(newListAgency, tmpAgency)
-			regionAgencies[tmpSlice[0]] = newListAgency
-			newListAgency = newListAgency[:0]	
+	var existId string
+	var tmpLine string
+	var editedFile []string
+
+	for fileScanner.Scan() {
+		tmpLine = fileScanner.Text() + "\n"
+		existId = strings.Split(tmpLine, ",")[1]
+		if existId == strconv.Itoa(id) {
+			tmpLine = fmt.Sprintf("%s,%d,%s,%s,%s,%s,%d\n",
+				region, agency.Id, agency.Name, agency.Address, agency.phoneNumber, agency.registeryDate, agency.numberEmployee)
+		}
+		editedFile = append(editedFile, tmpLine)
+	}
+
+	file.Close()
+	os.Remove("data.txt")
+	// what is the best solution ??????
+	os.Create("data.txt")
+	file, err = os.OpenFile("./data.txt", os.O_WRONLY, 0600)
+	if err != nil {
+		return err
+	}
+	for _, v := range editedFile {
+		if _, err = file.WriteString(v); err != nil {
+			return err
 		}
 	}
 	file.Close()
 	return nil
 }
-func calcId() int{
+func status(region string) (int, int) {
+	numberAllEmploye := 0
+	for _, v := range regionAgencies[region] {
+		numberAllEmploye += v.numberEmployee
+	}
+	return len(regionAgencies[region]), numberAllEmploye
+}
+func textFileToMapStructure() error {
+	file, err := os.OpenFile("data.txt", os.O_RDONLY, 0600)
+	if err != nil {
+		return err
+	}
+	fileScanner := bufio.NewScanner(file)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	var tmpSlice []string
+	var newListAgency []Agency
+	var tmpAgency Agency
+
+	for fileScanner.Scan() {
+		tmpSlice = strings.Split(fileScanner.Text(), ",")
+		tmpAgency.Id, err = strconv.Atoi(tmpSlice[1])
+		if err != nil {
+			return err
+		}
+		tmpAgency.Name = tmpSlice[2]
+		tmpAgency.Address = tmpSlice[3]
+		tmpAgency.phoneNumber = tmpSlice[4]
+		tmpAgency.registeryDate = tmpSlice[5]
+		tmpAgency.numberEmployee, err = strconv.Atoi(tmpSlice[6])
+		if err != nil {
+			return err
+		}
+		if _, ok := regionAgencies[tmpSlice[0]]; ok {
+			regionAgencies[tmpSlice[0]] = append(regionAgencies[tmpSlice[0]], tmpAgency)
+		} else {
+			newListAgency = append(newListAgency, tmpAgency)
+			regionAgencies[tmpSlice[0]] = newListAgency
+			newListAgency = nil
+		}
+	}
+	file.Close()
+	return nil
+}
+func calcId() int {
 	lastId := 0
-	for _, v := range regionAgencies{
+	for _, v := range regionAgencies {
 		lastId += len(v)
 	}
 	return lastId
 }
-func main(){
+func main() {
 	regionAgencies = make(map[string][]Agency)
+
 	command := flag.String("command", "", "choose the best command")
 	region := flag.String("region", "", "choose the region you want")
 	flag.Parse()
-	
-	err := textFileToMap()
-	if err != nil{
+
+	err := textFileToMapStructure()
+	if err != nil {
 		log.Fatal(err)
 	}
 
 	lastId := calcId()
 
-	switch *command{
-		case "list":{
+	switch *command {
+	case "list":
+		{
 			fRegion, err := checkFlagRegionAndExist(*region)
 			if err != nil {
 				log.Fatal(err)
@@ -176,17 +188,18 @@ func main(){
 
 			var listAgency []Agency
 			listAgency = list(fRegion)
-			for agen := range listAgency{
+			for agen := range listAgency {
 				fmt.Printf("Name of Agency is: %s\n", listAgency[agen].Name)
 			}
 		}
-		case "get":{
+	case "get":
+		{
 			fRegion, err := checkFlagRegionAndExist(*region)
 			if err != nil {
 				log.Fatal(err)
 			}
 			var inputId int
-			fmt.Printf("Please enter ID of Agency: ",)
+			fmt.Printf("Please enter ID of Agency: ")
 			fmt.Scanln(&inputId)
 			chooseAgency, err := get(fRegion, inputId)
 			if err != nil {
@@ -194,14 +207,15 @@ func main(){
 			}
 			fmt.Println("==============================================")
 			fmt.Println("region is:", fRegion,
-						"\nID of Agency is:", chooseAgency.Id,
-						"\nName of Agency is:", chooseAgency.Name,
-						"\nAddress of Agency is:", chooseAgency.Address,
-						"\nPhone Number of Agency is:", chooseAgency.phoneNumber,
-						"\nRegistery Date is:", chooseAgency.registeryDate,
-						"\nNumber of employee is:", chooseAgency.numberEmployee)
+				"\nID of Agency is:", chooseAgency.Id,
+				"\nName of Agency is:", chooseAgency.Name,
+				"\nAddress of Agency is:", chooseAgency.Address,
+				"\nPhone Number of Agency is:", chooseAgency.phoneNumber,
+				"\nRegistery Date is:", chooseAgency.registeryDate,
+				"\nNumber of employee is:", chooseAgency.numberEmployee)
 		}
-		case "create":{
+	case "create":
+		{
 			fRegion, err := checkFlagRegion(*region)
 			if err != nil {
 				log.Fatal(err)
@@ -218,19 +232,20 @@ func main(){
 			fmt.Scanln(&newAgency.registeryDate)
 			fmt.Print("Number of employee: ")
 			fmt.Scanln(&newAgency.numberEmployee)
-			
+
 			err = create(fRegion, newAgency)
 			if err != nil {
 				log.Fatal(err)
 			}
 		}
-		case "edit":{
+	case "edit":
+		{
 			fRegion, err := checkFlagRegionAndExist(*region)
 			if err != nil {
 				log.Fatal(err)
 			}
 			var inputId int
-			fmt.Printf("Please enter ID of Agency: ",)
+			fmt.Printf("Please enter ID of Agency: ")
 			fmt.Scanln(&inputId)
 			chooseAgency, err := get(fRegion, inputId)
 			if err != nil {
@@ -239,10 +254,13 @@ func main(){
 
 			var tmpAgency Agency
 			checkValue := func(new string, old string) string {
-				if new == "N" || new == "No" || new == "NO" || new == "no" || new == "n" {return old
-				} else {return new}
+				if new == "N" || new == "No" || new == "NO" || new == "no" || new == "n" {
+					return old
+				} else {
+					return new
+				}
 			}
-			
+
 			tmpAgency.Id = chooseAgency.Id
 			var tmp string
 			fmt.Printf("Name of Agency is: %s, Are you change it?(No or new_value): ", chooseAgency.Name)
@@ -257,15 +275,13 @@ func main(){
 			fmt.Scanln(&tmp)
 			tmpAgency.phoneNumber = checkValue(tmp, chooseAgency.phoneNumber)
 
-
 			fmt.Printf("Registery Date is: %s, Are you change it?(No or new_value): ", chooseAgency.registeryDate)
 			fmt.Scanln(&tmp)
 			tmpAgency.registeryDate = checkValue(tmp, chooseAgency.registeryDate)
 
-
 			fmt.Printf("Number of employee is: %d, Are you change it?(No or new_value): ", chooseAgency.numberEmployee)
 			fmt.Scanln(&tmp)
-			tmpAgency.numberEmployee, err = strconv.Atoi(checkValue(tmp,strconv.Itoa(chooseAgency.numberEmployee)))
+			tmpAgency.numberEmployee, err = strconv.Atoi(checkValue(tmp, strconv.Itoa(chooseAgency.numberEmployee)))
 			if err != nil {
 				log.Fatal(err)
 			}
@@ -274,17 +290,19 @@ func main(){
 			if err != nil {
 				log.Fatal(err)
 			}
-			
+
 		}
-		case "status":{
+	case "status":
+		{
 			fRegion, err := checkFlagRegionAndExist(*region)
 			if err != nil {
 				log.Fatal(err)
 			}
 			numberAllAgency, numberAllEmploye := status(fRegion)
 			fmt.Printf("in region %s, number of total Agency is: %d, number of total Employee is: %d\n",
-			 fRegion, numberAllAgency, numberAllEmploye)
+				fRegion, numberAllAgency, numberAllEmploye)
 		}
-		default: log.Fatalf("Error occurd!! Command %s not Found", *command)
+	default:
+		log.Fatalf("Error occurd!! Command %s not Found", *command)
 	}
 }
