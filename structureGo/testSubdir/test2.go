9e90edeb2536a4054d5ef180d38f477957cc6f4f
armiinjp <arminjp2000@gmail.com>
2023-03-15 12:08:34 +0330

    learning episoid 6 and write text

--- structureGo/testSubdir/test2.go
+++ structureGo/testSubdir/test2.go
@@ -3,6 +3,9 @@ package testSubdir
 import "fmt"
 
 var B = 4
+func init(){
+	fmt.Println("salam in test2")
+}
 func test(){
 	fmt.Println("you are in test2.go")
 }
--- structureGo/testSubdir/test3.go
+++ structureGo/testSubdir/test3.go
@@ -3,6 +3,9 @@ package testSubdir
 import "fmt"
 
 var A = 3
+func init(){
+	fmt.Println("salam in test3")
+}
 func Te()string {
 	test()
 	return fmt.Sprint("this func in the test3.go in the testSubdir package")
